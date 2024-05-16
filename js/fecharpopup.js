67c6ec138a514db4e0fe9e142e14b9781ff5d7fc
edubto <eduardoaferreira20@outlook.com>
2024-05-16 18:05:55 -0300

    2

---
+++ js/fecharpopup.js
@@ -0,0 +1,12 @@
+
+
+const fecharPopupButton = document.getElementById('fecharPopupButton');
+
+// Adicionando um ouvinte de evento de clique ao botão
+fecharPopupButton.
+fecharPopu
+addEventListener('click', function() {
+    
+    closeEditPopu
+closeEditPopup(); // Chamando a função closeEditPopup() quando o botão for clicado
+});
