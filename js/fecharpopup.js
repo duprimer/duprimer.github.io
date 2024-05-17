de2a54627b774e0a3797d6343b2e82d6f8d86d9a
geodso2024 <gdso.silva@gmail.com>
2024-05-17 17:26:48 -0300

    1

---
+++ js/fecharpopup.js
@@ -0,0 +1,11 @@
+
+
+const fecharPopupButton = document.getElementById('fecharPopupButton');
+
+// Adicionando um ouvinte de evento de clique ao botão
+
+addEventListener('click', function() {
+    
+    closeEditPopu
+closeEditPopup(); // Chamando a função closeEditPopup() quando o botão for clicado
+});
